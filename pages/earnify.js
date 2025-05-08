ad02a04250017c612eb02ade240572968a90bdda
Varshni <varshnik@gmail.com>
2025-05-08 10:46:50 -0500

    updated feature showcase

--- pages/earnify.js
+++ pages/earnify.js
@@ -67,20 +67,9 @@ const Earnify = () => {
 
             <h2 className="mb-2 laptop:mb-6 text-2xl laptop:text-3xl font-bold">Feature Showcase</h2>
             <img className="w-full h-auto mb-4 laptop:mb-4 rounded-lg object-cover" src="/images/earnify/feature_screens_1.svg" alt="feature 1"></img>
-            <p className="mb-4 laptop:mb-14 opacity-80 leading-7 font-normal text-base laptop:text-xl"><b>Benzie weekly challenge</b> - The Benzie section on the rewards page allows the user to view all the Benzie challenges for that week. Once completed, they can earn a new accessory to style Benzie.</p>
             <img className="w-full h-auto mb-4 laptop:mb-4 rounded-lg object-cover" src="/images/earnify/feature_screens_2.svg" alt="feature 2"></img>
-            <p className="mb-4 laptop:mb-4 opacity-80 leading-7 font-normal text-base laptop:text-xl"><b>Surprise pop up challenge</b> - The Benzie section on the rewards page allows the user to view all the Benzie challenges for that week. Once completed, they can earn a new accessory to style Benzie.</p>
-            <p className="mb-4 laptop:mb-14 opacity-80 leading-7 font-normal text-base laptop:text-xl"><b>Scan to pay wallet</b> - The barcode section on the wallet page allows the user to scan the barcode for purchases made with app funds & earn points. If the surprise pop up is redeemed, it will show up on the wallet like pictured above, if not, the wallet page will only feature the barcode.</p>
-            <img className="w-full h-auto mb-4 laptop:mb-4 rounded-lg object-cover" src="/images/earnify/feature_screens_3.svg" alt="feature 3"></img>
-            <p className="mb-4 laptop:mb-4 opacity-80 leading-7 font-normal text-base laptop:text-xl"><b>Gas price lock</b> - Once a user locks the price on a specific location, it is indicated in red. This allows the user to “lock in” the current price of gas to be used on a future purchase (it will only be valid for a certain amount of time).</p>
-            <p className="mb-4 laptop:mb-14 opacity-80 leading-7 font-normal text-base laptop:text-xl"><b>Favorite locations</b> - By saving certain locations as favorite, the user can easily view it on the fuel page.</p>
-            <img className="w-full h-auto mb-4 laptop:mb-4 rounded-lg object-cover" src="/images/earnify/feature_screens_4.svg" alt="feature 4"></img>
-            <p className="mb-4 laptop:mb-4 opacity-80 leading-7 font-normal text-base laptop:text-xl"><b>Order</b> - This page allows the user to view items that are available to order at a specific along with their nutritional info.</p>
-            <p className="mb-4 laptop:mb-14 opacity-80 leading-7 font-normal text-base laptop:text-xl"><b>Pickup</b> - This page allows the user to order items from a specific store. They can choose any item from the order page or order your favorite combos that are saved into the app.</p>
-            <img className="w-full h-auto mb-4 laptop:mb-4 rounded-lg object-cover" src="/images/earnify/feature_screens_5.svg" alt="feature 5"></img>
-            <p className="mb-4 laptop:mb-4 opacity-80 leading-7 font-normal text-base laptop:text-xl"><b>Home</b> - This page is similar to earnify’s current design while incorporating Benzie to remind the user about Benzie challenges and rewards. There is also a streak feature to keep the user engaged.</p>
-            <p className="mb-4 laptop:mb-14 opacity-80 leading-7 font-normal text-base laptop:text-xl"><b>Notifications</b> - Timely notifications remind the user of deals & rewards to redeem.</p>
-
+            <img className="w-full h-auto mb-4 laptop:mb-14 rounded-lg object-cover" src="/images/earnify/feature_screens_3.svg" alt="feature 3"></img>
+            
             <h2 className="mb-2 laptop:mb-14 text-2xl laptop:text-3xl"><i>Final prototype link coming soon!</i></h2>
 
             <h3 className="mt-8 mb-4 opacity-50 text-sm laptop:text-lg">My Takeways</h3>

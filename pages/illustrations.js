c9b1085af8175febbf9319365250697675cd06aa
Varshni <varshnik@gmail.com>
2025-06-12 12:16:09 -0500

    added more illustrations

--- pages/illustrations.js
+++ pages/illustrations.js
@@ -17,15 +17,19 @@ const Illustrations = () => {
                 <h1 className="mx-auto mob:p-2 text-bold text-3xl laptop:text-4xl w-full" >Check out some of my illustrations!</h1>
                 <div class="grid grid-cols-2 mt-8 laptop:mt-10 md:grid-cols-3 gap-5 laptop:gap-20">
                     <div>
+                        <img class="h-auto max-w-full rounded" src="/images/illustrations/idCard.svg" alt=""/>
+                        {/* <p class="mt-2 text-xs laptop:mt-4 laptop:text-lg">The Backwaters, 2020 · <i>Acrylic</i></p> */}
+                    </div>
+                     <div>
+                        <img class="h-auto max-w-full rounded" src="/images/illustrations/varsh_stamp_sticker.svg" alt=""/>
+                        {/* <p class="mt-2 text-xs laptop:mt-4 laptop:text-lg">A Calm Morning, 2024 · <i>Acrylic</i></p> */}
+                    </div>
+                    <div>
                         <img class="h-auto max-w-full rounded" src="/images/illustrations/wireframe_sticker.svg" alt=""/>
                         {/* <p class="mt-2 text-xs laptop:mt-4 laptop:text-lg">The Backwaters, 2020 · <i>Acrylic</i></p> */}
                     </div>
                     <div>
                         <img class="h-auto max-w-full rounded" src="/images/illustrations/varsh_conf_sticker_two.svg" alt=""/>
-                        {/* <p class="mt-2 text-xs laptop:mt-4 laptop:text-lg">A Calm Morning, 2024 · <i>Acrylic</i></p> */}
-                    </div>
-                    <div>
-                        <img class="h-auto max-w-full rounded" src="/images/illustrations/varsh_stamp_sticker.svg" alt=""/>
                         {/* <p class="mt-2 text-xs laptop:mt-4 laptop:text-lg">A Calm Morning, 2024 · <i>Acrylic</i></p> */}
                     </div>
                     <div>

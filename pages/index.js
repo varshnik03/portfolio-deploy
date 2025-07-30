74182c37b7afd53f830fdd5167e68854ad7d0062
Varshni <varshnik@gmail.com>
2025-07-29 20:55:27 -0500

    added researcher to bio

--- pages/index.js
+++ pages/index.js
@@ -74,13 +74,13 @@ export default function Home() {
               </h1>
               <h1
                 ref={textTwo}
-                className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-6xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
+                className="text-2xl tablet:text-5xl laptop:text-5xl laptopl:text-5xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
               >
                 {data.headerTaglineTwo}
               </h1>
               <h1
                 ref={textThree}
-                className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-6xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
+                className="text-2xl tablet:text-5xl laptop:text-5xl laptopl:text-5xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
               >
                 {data.headerTaglineThree}
               </h1>
